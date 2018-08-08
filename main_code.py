0db9b03b507c832ad42f3fcf6b05bf03aa3d02de
NexInfinite <37962677+NexInfinite@users.noreply.github.com>
2018-08-08 18:00:31 +0100

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -41,7 +41,7 @@ async def on_ready():
     print(client.user.name)
     print('--------------')
     msg = f'```Hello, Im Bored bot v1.0.5, How bored are u?```'
-    await client.send_message(client.get_channel('447354418250121216'), msg)
+    await client.send_message(client.get_channel('412723545316261890'), msg)
     await client.change_presence(game=discord.Game(name='^help'))
 
 client.run(os.getenv('TOKEN'))
