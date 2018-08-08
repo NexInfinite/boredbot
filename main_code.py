ffdeb9de7dc6633351b133da1299bb982eabd465
NexInfinite <37962677+NexInfinite@users.noreply.github.com>
2018-08-08 13:18:10 +0100

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -9,7 +9,9 @@ msg = ''
 
 client = discord.Client()
 
-bordem_messages = ['Eat some food.', 'Dab on them haters.', 'Watch a movie.', "Watch nex's stream.", 'Do some launchpadding?', "Wouldn't you like to know weather boi!"]
+bordem_messages = ['Eat some food.', 'Dab on them haters.', 'Watch a movie.', "Watch nex's stream.", 'Do some launchpadding?',
+                   "Wouldn't you like to know weather boi!", "play a game", "yOu NeEd To WoKe", "watch tv!", "play OUTSIIIIIIDE",
+                   "play a boardgame", "listen to: agiauegbae LET THE GAMES BEGIN IS AMAZE"]
 
 @client.event
 async def on_message(message):
@@ -22,33 +24,10 @@ async def on_message(message):
         msg = f'{user}, This is what you can do to not be bored: {random.choice(bordem_messages)}'
         await client.send_message(message.channel, msg)
 
-    if message.content.lower().startswith('^addmsg'):
+    if message.content.lower().startswith('^bored_list'):
         user = '{0.author.mention}'.format(message)
-        role_names = [role.name for role in message.author.roles]
-        if "leader" in role_names:
-            print('working!!!!!!!!!!')
-            #split_message = shlex.split(message.content)
-            #print(split_message[1:])
-            #if len(split_message) < 2:
-                #msg = f'```ERROR, use: ^boredadd XXXXXXXXXXX. The XXXXX must be replaced with your own message!```'
-                #await client.send_message(message.channel, msg)
-            #else:
-                #response = insert_msg(
-                    #message_given=split_message[1:]
-                #)
-                #if response == 0:
-                    #msg = f'{user} successfully added the message *{" ".join(split_message[1:])}* to the database!'
-                    #await client.send_message(message.channel, msg)
-                #elif response == 1:
-                    #msg = f'{user} the response {" ".join(split_message[1:])} is already in the database, try another response'
-                    #await client.send_message(message.channel, msg)
-        else:
-            print('nein')
-            #msg = f'{user}, this command is down rn. I am working on it and cant have people doing stuff with it.'
-            #await client.send_message(message.channel, msg)
-            #print(message.author)
-
-
+        msg = f'{user} the bored messages are: ```{bordem_messages}```'
+        await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
@@ -56,7 +35,7 @@ async def on_ready():
     print(client.user.name)
     print('--------------')
     msg = f'```Hello, Im Bored bot v1.0.5, How bored are u?```'
-    await client.send_message(client.get_channel('412723545316261890'), msg)
+    await client.send_message(client.get_channel('447354418250121216'), msg)
     await client.change_presence(game=discord.Game(name='^help'))
 
 client.run(os.getenv('TOKEN'))
